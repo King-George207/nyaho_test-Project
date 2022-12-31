1a9de89cf8e0ebbbee6e93e5c523abc8219c60bf
KING GEORGE <121632236+King-George207@users.noreply.github.com>
2022-12-31 08:49:16 -0800

    fixed image error during register
    fixed image error during register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,6 +44,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'image_url' => ''
         ]);
         
         //save patient information
