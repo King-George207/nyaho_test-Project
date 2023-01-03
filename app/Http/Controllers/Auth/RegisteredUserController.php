5f88ac2b6a7ece943ce3fe60a8d51f14ac752767
KING GEORGE <121632236+King-George207@users.noreply.github.com>
2023-01-03 20:53:15 +0000

    updated signup

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,6 +9,7 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 
@@ -40,21 +41,97 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'image_url' => ''
-        ]);
-        
+        //DB::raw("INSERT INTO users (name, email, password) VALUES()")
+        DB::table('users')
+->insert([
+    'name' => $request->name,
+    'email' => $request->email,
+    'password' => Hash::make($request->password),
+]);
+
+
         //save patient information
-        PatientRecord::create([
+        $user = User::orderBy('id','DESC')->first();
+        DB::table('patient_records')
+        ->insert([
             'user_id' => $user->id,
             'gender' => $request->gender,
             'phone' => $request->phone,
             'name' => $request->name,
             'email' => $request->email,
         ]);
+
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\PatientRecord;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        //DB::raw("INSERT INTO users (name, email, password) VALUES()")
+        DB::table('users')
+->insert([
+    'name' => $request->name,
+    'email' => $request->email,
+    'password' => Hash::make($request->password),
+]);
+
+
+        //save patient information
+        $user = User::orderBy('id','DESC')->first();
+        DB::table('patient_records')
+        ->insert([
+            'user_id' => $user->id,
+            'gender' => $request->gender,
+            'phone' => $request->phone,
+            'name' => $request->name,
+            'email' => $request->email,
+        ]);
+
 
         event(new Registered($user));
 
